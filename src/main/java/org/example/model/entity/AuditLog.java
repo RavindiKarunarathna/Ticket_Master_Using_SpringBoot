b7a5ba360877b23e1b16f61991112b53e74b8b8e
RavindiKarunarathna <ravindiicet@gmail.com>
2026-01-20 14:43:23 +0530

    Edit AuditLog Entity class

--- src/main/java/org/example/model/entity/AuditLog.java
+++ src/main/java/org/example/model/entity/AuditLog.java
@@ -1,4 +1,20 @@
 package org.example.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class AuditLog {
+    @Id @GeneratedValue private Long id;
+    private Long userID;
+    private String action;
+    private String details;
+    private LocalDateTime timestamp = LocalDateTime.now();
 }
