88966a1ad7f3019e761e334ee00a902dd829b8c1
RavindiKarunarathna <ravindiicet@gmail.com>
2026-01-20 15:19:27 +0530

    Edit AuditLogRepository class

--- src/main/java/org/example/repository/AuditLogRepository.java
+++ src/main/java/org/example/repository/AuditLogRepository.java
@@ -1,4 +1,7 @@
 package org.example.repository;
 
-public interface AuditLog {
+import org.example.model.entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
 }
