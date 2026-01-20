da3b3c614911991ec9825b7f8e37ab2da9223027
RavindiKarunarathna <ravindiicet@gmail.com>
2026-01-20 14:16:01 +0530

    Edit User Entity class

--- src/main/java/org/example/model/entity/User.java
+++ src/main/java/org/example/model/entity/User.java
@@ -1,5 +1,15 @@
 package org.example.model.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY) private Long id;
+    private String name;
+    @Enumerated (EnumType.STRING) private Tier tier;
+    public enum Tier{REGULAR, VIP, PLATINUM}
 }
