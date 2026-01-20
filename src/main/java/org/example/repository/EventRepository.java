2e6aae16d6e9caccf0e10ba94dc706c4a2277643
RavindiKarunarathna <ravindiicet@gmail.com>
2026-01-20 15:13:47 +0530

    Edit EventRepository class

--- src/main/java/org/example/repository/EventRepository.java
+++ src/main/java/org/example/repository/EventRepository.java
@@ -1,4 +1,7 @@
 package org.example.repository;
 
-public interface EventRepository {
+import org.example.model.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
 }
